4b5268fdfc52aeaff573a13fc484f5dc8663ecc7
Eric <bonderic44@gmail.com>
2024-03-09 22:01:28 +0545

    fix : using Navigate by replacing useNavigate hook

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,26 +3,18 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  useNavigate,
+  Navigate,
 } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 
 const RoutesComponent = () => {
-  let navigate = useNavigate();
-
   return (
     <Routes>
       <Route path="/" element={<Users />} />
       <Route path="/places/new" element={<NewPlace />} />
-      <Route
-        path="*"
-        element={() => {
-          navigate("/", { replace: true });
-          return null;
-        }}
-      />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
