a2d613d3e2f52c53e03846c27e2ed2f733c938c7
Eric <bonderic44@gmail.com>
2024-03-16 17:33:00 +0545

    feat : UserPlaces route added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,12 +8,14 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/Userplaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<Users />} />
+      <Route path="/:userId/places" element={<UserPlaces />} />
       <Route path="/places/new" element={<NewPlace />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
