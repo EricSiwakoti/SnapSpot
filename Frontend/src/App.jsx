c87d2eb8de9027cc63669f3307dd6f40be793ba8
Eric <bonderic44@gmail.com>
2024-03-23 00:51:58 +0545

    chore : Rename Userplaces.jsx to UserPlaces.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/Userplaces";
+import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const RoutesComponent = () => {
