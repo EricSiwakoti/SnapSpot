5bf345d62b50a0417d7019ab1acf234af3ad8c47
Eric <bonderic44@gmail.com>
2024-06-06 10:37:53 +0545

    feat : added value and valid props to  inputState

--- Frontend/src/shared/components/FormElements/Input.jsx
+++ Frontend/src/shared/components/FormElements/Input.jsx
@@ -22,9 +22,9 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: "",
+    value: props.value || "",
     isTouched: false,
-    isValid: false,
+    isValid: props.valid || false,
   });
 
   const { id, onInput } = props;
@@ -33,6 +33,7 @@ const Input = (props) => {
   useEffect(() => {
     onInput(id, value, isValid);
   }, [id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
     dispatch({
       type: "CHANGE",
