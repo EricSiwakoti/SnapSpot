7a65d30d1e5c621401a3d4184eefcc6e9f96d1c3
Eric <bonderic44@gmail.com>
2024-06-06 10:15:14 +0545

    feat : UpdatePlace component added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const RoutesComponent = () => {
@@ -17,6 +18,7 @@ const RoutesComponent = () => {
       <Route path="/" element={<Users />} />
       <Route path="/:userId/places" element={<UserPlaces />} />
       <Route path="/places/new" element={<NewPlace />} />
+      <Route path="/places/:placeId" element={<UpdatePlace />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
