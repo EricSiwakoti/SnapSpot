0ae746cca9c0dd89b731230b2fb8ca75f2eaea60
Eric <bonderic44@gmail.com>
2024-06-07 09:27:37 +0545

    feat : auth route added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,15 +10,17 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const RoutesComponent = () => {
   return (
     <Routes>
-      <Route path="/" element={<Users />} />
-      <Route path="/:userId/places" element={<UserPlaces />} />
-      <Route path="/places/new" element={<NewPlace />} />
+      <Route path="/" element={<Users />} exact />
+      <Route path="/:userId/places" element={<UserPlaces />} exact />
+      <Route path="/places/new" element={<NewPlace />} exact />
       <Route path="/places/:placeId" element={<UpdatePlace />} />
+      <Route path="/auth" element={<Auth />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
