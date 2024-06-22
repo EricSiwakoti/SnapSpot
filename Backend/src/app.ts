0587c63eddd599777356335264eadaa9b8ba6412
Eric <bonderic44@gmail.com>
2024-06-22 15:25:22 +0545

    feat : implemented basic routing

---
+++ Backend/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import placesRoutes from "./routes/places-routes";
+
+const app = express();
+
+app.use("/api/places", placesRoutes);
+
+app.listen(5000);
