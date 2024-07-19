1278e35efd6ba4d2df14a967c705089aa0808745
Eric <bonderic44@gmail.com>
2024-07-19 13:09:03 +0545

    feat : http error model added

---
+++ Backend/src/models/http-error.ts
@@ -0,0 +1,8 @@
+export class HttpError extends Error {
+  code: number;
+
+  constructor(message: string, errorCode: number) {
+    super(message); // Add a "message" property
+    this.code = errorCode; // Add a "code" property
+  }
+}
