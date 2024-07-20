e34cf454ad167afc1bbc1995f02821885d5ea068
Eric <bonderic44@gmail.com>
2024-07-21 01:10:28 +0545

    feat : added userRoutes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import placesRoutes from "./routes/places-routes";
+import usersRoutes from "./routes/users-routes";
 import HttpError from "./models/http-error";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   const error = new HttpError("Could not find this route.", 404);
