b0341a3beae77eab469064f290baa2b7f954aeb9
Eric <bonderic44@gmail.com>
2024-07-20 14:40:59 +0545

    feat : imported HttpError model

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
-
 import placesRoutes from "./routes/places-routes";
+import HttpError from "./models/http-error";
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
