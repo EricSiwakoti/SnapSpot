4675cf8c656aaf20a18336897a4da927623894ba
Eric <bonderic44@gmail.com>
2024-07-25 02:10:58 +0545

    feat : added place schema

---
+++ Backend/src/models/place.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+interface ILocation {
+  lat: number;
+  lng: number;
+}
+
+interface IPlace {
+  title: string;
+  description: string;
+  image: string;
+  address: string;
+  location: ILocation;
+  creator: string;
+}
+
+const placeSchema = new mongoose.Schema<IPlace>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { type: String, required: true },
+});
+
+const Place = mongoose.model<IPlace>("Place", placeSchema);
+
+export default Place;
