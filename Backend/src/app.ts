857d206c3e4198dc3a8a410001afc05771437f9a
Eric <bonderic44@gmail.com>
2024-08-07 01:38:14 +0545

    feat : CORS error handeled

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,6 +8,17 @@ import HttpError from "./models/http-error";
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
