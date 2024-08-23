ebad8adbc20f06e25be61f2a4bef31876ffaa65d
Eric <bonderic44@gmail.com>
2024-08-24 03:17:46 +0545

    feat : .\Backend\src\middleware\check-auth.ts

---
+++ Backend/src/middleware/check-auth.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import HttpError from "../models/http-error";
+
+interface AuthRequest extends Request {
+  userData?: { userId: string };
+}
+
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization?.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error("Authentication failed!");
+    }
+
+    const decodedToken = jwt.verify(
+      token,
+      "supersecret_dont_share"
+    ) as jwt.JwtPayload;
+    req.userData = { userId: decodedToken.userId as string };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 403);
+    return next(error);
+  }
+};
+
+export default authMiddleware;
