c93d0261c340f49070cb4da573af5b55098a55d5
Eric <bonderic44@gmail.com>
2024-08-30 00:21:04 +0545

    fix : update asset URL in UserItem component

--- Frontend/src/user/components/UserItem.jsx
+++ Frontend/src/user/components/UserItem.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
@@ -10,7 +9,10 @@ const UserItem = (props) => {
       <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-            <Avatar image={props.image} alt={props.name} />
+            <Avatar
+              image={`${process.env.REACT_APP_ASSET_URL}/${props.image}`}
+              alt={props.name}
+            />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
