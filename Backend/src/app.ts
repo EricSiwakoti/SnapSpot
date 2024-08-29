4a80ae44895f7bd25daa2fc011636edc9542053b
Eric <bonderic44@gmail.com>
2024-08-29 23:46:06 +0545

    feat : Enhance app configuration and error handling with dotenv, CORS middleware, and improved MongoDB connection

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,50 +6,55 @@ import path from "path";
 import placesRoutes from "./routes/places-routes";
 import usersRoutes from "./routes/users-routes";
 import HttpError from "./models/http-error";
+import dotenv from "dotenv";
+import cors from "cors";
 
+interface CustomError extends Error {
+  statusCode?: number;
+}
+
+dotenv.config();
 const app = express();
+const PORT: string | number = process.env.PORT ?? 5000;
+const MONGO_URI: string | undefined = process.env.MONGO_URI ?? "";
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
-
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   const error = new HttpError("Could not find this route.", 404);
-  throw error;
+  next(error);
 });
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
+app.use(
+  (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    if (req.file) {
+      fs.unlink(req.file.path, (err) => {
+        console.log("Error deleting file", err);
+      });
+    }
+    if (res.headersSent) {
+      return next(error);
+    }
+    res
+      .status(error.statusCode ?? 500)
+      .json({ message: error.message || "An unknown error occurred!" });
   }
-  if (res.headersSent) {
-    return next(error);
-  }
-  res
-    .status(500)
-    .json({ message: error.message || "An unknown error occurred!" });
-});
+);
 
 mongoose
-  .connect("your MongoURI connection string", {})
+  .connect(MONGO_URI)
   .then(() => {
-    app.listen(5000);
+    console.log("Connected to database successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Error connecting to database", err);
+    process.exit(1);
   });
