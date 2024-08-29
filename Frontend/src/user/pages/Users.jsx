3169aec12c8c9ab3afeb3b671a74bc4f98f9d52f
Eric <bonderic44@gmail.com>
2024-08-30 00:24:53 +0545

    fix : update backend URL in Users component

--- Frontend/src/user/pages/Users.jsx
+++ Frontend/src/user/pages/Users.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
@@ -12,7 +12,7 @@ const Users = () => {
     const fetchUsers = async () => {
       try {
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users"
+          process.env.REACT_APP_BACKEND_URL + "/users"
         );
 
         setLoadedUsers(responseData.users);
