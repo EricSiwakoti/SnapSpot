50ebd14afc1ea0798e3c368b16ccf99f01e4b60e
Eric <bonderic44@gmail.com>
2024-08-30 00:07:32 +0545

    feat : Use environment variables for backend and asset URLs, and add authentication to delete request

--- Frontend/src/places/components/PlaceItem.jsx
+++ Frontend/src/places/components/PlaceItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import { useState, useContext } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
@@ -29,8 +29,12 @@ const PlaceItem = (props) => {
     setShowConfirmModal(false);
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${props.id}`,
-        "DELETE"
+        process.env.REACT_APP_BACKEND_URL + `/places/${props.id}`,
+        "DELETE",
+        null,
+        {
+          Authorization: `Bearer ${auth.token}`,
+        }
       );
       props.onDelete(props.id);
     } catch (err) {
@@ -81,7 +85,7 @@ const PlaceItem = (props) => {
           {isLoading && <LoadingSpinner asOverlay />}
           <div className="place-item__image">
             <img
-              src={`http://localhost:5000/${props.image}`}
+              src={`${process.env.REACT_APP_ASSET_URL}/${props.image}`}
               alt={props.title}
             />
           </div>
